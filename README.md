3983871a3eed3d9a12a631a1e1272e9b61a2d458
Johannes Allen Sanchez <johannes.sanchez@oportun.com>
2022-09-01 22:09:56 -0500

    Base

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # build-sl
 Build Serverless
+
+# Way to deploy lambda
+1. sam build
+2. sam package
+3. sam deploy --guided
+
+# Test locally
+sam local start-api
+
+* Local request:
+http://127.0.0.1:3000/status?personId=5
---
+++ buildsl/status.py
@@ -0,0 +1,16 @@
+import json
+
+import boto3
+# import requests
+
+
+def lambda_handler(event, context):
+
+    personId = event['queryStringParameters']['personId']
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "personId": personId + " from Lambda" ,
+        }),
+    }
